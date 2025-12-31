a01e412b0baf75de7addeb79bda5b95102e19d2f
Munna Kumar <munnakumar68768@gmail.com>
2026-01-01 00:25:52 +0530

    changes

---
+++ laravel-login-system/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    // REGISTER API
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully',
+            'data' => $user
+        ]);
+    }
+
+    // LOGIN API
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Login successful',
+            'user' => Auth::user()
+        ]);
+    }
+}
